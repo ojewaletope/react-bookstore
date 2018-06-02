f840c999afbef15f89481add256cf8a6e590ab93
Topeojewale <topeojewale@gmail.com>
2018-06-02 22:49:03 +0100

    Added functionality to the form input and search button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { FormGroup, InputGroup, FormControl, Glyphicon, Button } from 'react-bootstrap'
 import './App.css';
 
 class App extends Component {
+  state = {
+    searchValue: ""
+  };
+  search() {
+    console.log(this.state.searchValue);
+  }
   render() {
     return (
-      
+      <div>
+        <h2>Book Explorer</h2>
+        <FormGroup>
+          <InputGroup>
+            <FormControl type="text" placeholder="Search for books here" onChange={e => this.setState({
+              searchValue: e.target.value
+            })} value={this.state.searchValue} />
+            <Button onClick={() => this.search()}>Search</Button>
+          </InputGroup>
+        </FormGroup>
+      </div>
     );
   }
 }
