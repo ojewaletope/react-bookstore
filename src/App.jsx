ed9f2cf515535b46be096614a802cf1cf61b4417
Topeojewale <topeojewale@gmail.com>
2018-06-02 23:49:26 +0100

    fetch data using the fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,25 @@ class App extends Component {
     searchValue: ""
   };
   search() {
-    console.log(this.state.searchValue);
+    const url = "https://www.googleapis.com/books/v1/volumes?q="
+    fetch(`${url}${this.state.searchValue}`, {method: "GET"})
+      .then(res => res.json())
+      .then(data => console.log(data.items));
   }
   render() {
     return (
-      <div>
+      <div className="container">
         <h2>Book Explorer</h2>
         <FormGroup>
           <InputGroup>
             <FormControl type="text" placeholder="Search for books here" onChange={e => this.setState({
               searchValue: e.target.value
-            })} value={this.state.searchValue} />
+            })} value={this.state.searchValue}
+              onKeyPress={e => {
+                if (e.key === 'Enter') {
+                  this.search()
+                }
+              }} />
             <Button onClick={() => this.search()}>Search</Button>
           </InputGroup>
         </FormGroup>
